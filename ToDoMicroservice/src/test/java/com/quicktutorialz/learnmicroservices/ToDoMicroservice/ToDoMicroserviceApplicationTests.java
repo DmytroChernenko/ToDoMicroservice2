a1b615eb469f704f4983a5e364c365e27ac38276
Dmytro <mitresko12@gmail.com>
2020-02-06 16:35:56 +0200

    initial

---
+++ ToDoMicroservice/src/test/java/com/quicktutorialz/learnmicroservices/ToDoMicroservice/ToDoMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quicktutorialz.learnmicroservices.ToDoMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
